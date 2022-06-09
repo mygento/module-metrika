1dad3536be2d531e475cbb398eeb35b4e3813628
Dmitrii Gromov <d.gromov@mygento.ru>
2022-06-09 19:17:43 +0300

    init 2.4

--- Observer/AddToCart.php
+++ Observer/AddToCart.php
@@ -2,7 +2,7 @@
 
 /**
  * @author Mygento Team
- * @copyright 2015-2020 Mygento (https://www.mygento.ru)
+ * @copyright 2015-2022 Mygento (https://www.mygento.ru)
  * @package Mygento_Metrika
  */
 
