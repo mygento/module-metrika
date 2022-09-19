9e544eba1e20e265f3e908a8737a617bd4a383ac
Dmitrii <d.gromov@mygento.ru>
2022-09-19 12:21:09 +0300

    init 2.4 (#7)
    * init 2.4
    
    * Delete .travis.yml
    
    * upd requires
    
    * phpstan.neon
    
    Co-authored-by: Nikita <luckyraul@users.noreply.github.com>

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -1,7 +1,7 @@
 <?php
 $header = <<<EOF
 @author Mygento Team
-@copyright 2015-2020 Mygento (https://www.mygento.ru)
+@copyright 2015-2022 Mygento (https://www.mygento.ru)
 @package Mygento_Metrika
 EOF;
 
