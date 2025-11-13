0cd06038e41da360ea3b6bc418855a1a323810b9
Savinov Dmitry <dimokz@gmail.com>
2025-11-13 14:20:55 +0300

    OD-1086 fix package info

--- CustomerData/Metrika.php
+++ CustomerData/Metrika.php
@@ -2,7 +2,7 @@
 
 /**
  * @author Mygento Team
- * @copyright 2015-2025 Mygento (https://www.mygento.ru)
+ * @copyright 2015-2025 Mygento (https://www.mygento.com)
  * @package Mygento_Metrika
  */
 
